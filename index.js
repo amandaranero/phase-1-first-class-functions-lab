0cbd9de905223421ee344ab659ccae36a841f69a
amanda ranero <amandaranero@gmail.com>
2023-02-22 00:30:23 -0500

    first passing test

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function(array){
+    const firstTwoDrivers = [...array]
+    firstTwoDrivers.splice(2,2);
+    return firstTwoDrivers
+}
+
+const returnLastTwoDrivers = function(array){
+    const lastTwoDrivers = [...array]
+    lastTwoDrivers.splice(0,2)
+    return lastTwoDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+    return function quad(fare){
+        return fare*num};
+    
+};
+
+
+const fareDoubler = function(fare){
+    return fare*2
+};
+
+const fareTripler = function(fare){
+    return fare*3
+}
+
+function selectDifferentDrivers(array,fn){
+    return fn(array)
+
+}
